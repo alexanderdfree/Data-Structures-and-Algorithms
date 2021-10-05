920c80c14fc9cc0f5ad5e878d91352f8e8fcca8a
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-10-05 14:18:40 -0700

    new organization

---
+++ U1/MergeSort.java
@@ -0,0 +1,24 @@
+public class MergeSort{
+   public static IntList merge(IntList sorted1, IntList sorted2){
+      /*Merge two sorted lists into one new sorted list
+      Input:
+         IntList sorted1: an Intlist sorted in ascending order
+         IntList sorted2: another sorted IntList
+      Output:
+         return: a new IntList containing all the entries in
+                 sorted1 and sorted2, sorted in ascending order
+      Side Effects: None, sorted1 and sorted2 are unchanged
+      Ex.
+      IntList list1 = new IntList();
+      list1.add(1);
+      list1.add(3);
+      list1.add(5);
+      IntList list2 = new IntList();
+      list2.add(2);
+      list2.add(4);
+      list1.toString() -> [1, 3, 5]
+      list2.toString() -> [2, 4]
+      merge(list1, list2) -> [1, 2, 3, 4, 5]
+      */
+   }
+}
